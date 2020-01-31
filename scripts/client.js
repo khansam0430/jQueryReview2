54855b87a6415558754e65707f3185c5ad700146
Samreen Khan <khansam0430@gmail.com>
2020-01-31 10:34:38 -0600

    button click for display Songs

--- scripts/client.js
+++ scripts/client.js
@@ -1,6 +1,22 @@
 $( document ).ready(onReady);
 
+//songs app.
+let songs= [];
+
 function onReady() {
     console.log('in onReady');
-    
+    $('#updateSongsButton').on('click', displaySongs);
 }//end onReady.
+
+function displaySongs() {
+    console.log('in displaySongs');
+    
+}//end displaySongs.
+
+/// - test song
+songs.push( {
+    artist: 'Red Hot Chili Peppers',
+    songs: 'Under the Bridge',
+    album: 'Blood Sugar Sex Magik',
+    year: 1991,
+});
