28479d7d83150eefe0d2a0d8712ab1de246598f3
Samreen Khan <khansam0430@gmail.com>
2020-02-01 16:20:35 -0600

    worked on jQueryReview2 in class lecture

--- scripts/client.js
+++ scripts/client.js
@@ -5,8 +5,8 @@ let songs= [];
 
 function onReady() {
     console.log('in onReady');
-    $('#addSongsButton').on('click', addSong);
-
+    $( '#addSongsButton' ).on('click', addSong);
+    $( '#songsOut' ).on('click', '.digButton', digSong);
 }//end onReady.
 
 function addSong() {
@@ -30,20 +30,37 @@ function addSong() {
     displaySongs();
 }//end addSong
 
+function digSong(){
+    console.log('in digSong');
+    let el = $( this ); //THIS clicked button.
+    el.text( 'Hate' );
+    //if dig, set to hate, otherwise set to dig
+    if (el.text()=== 'Dig'){
+        el.text( 'Hate' );
+    } //end if
+    else{
+        el.text( 'Dig' );
+    }//end else
+    //change style of parent
+    //select parent element
+    let parent = $( this ).parent();
+    console.log( 'my parent is:', parent );
+    parent.toggleClass( 'hate' );
+    
+}//end digSong
+
 function displaySongs() {
     console.log('in displaySongs');
-    //target an output element by ID
+    //target an output element by ID (el just is short for element, also el just exists in function so is reusable outside of function)
     let el = $( '#songsOut' );
     //empty out element
     el.empty();
     //loop through songs array
     for (let i = 0; i<songs.length; i++){
         //append each song to DOM
-        el.append(`<li>${ songs[i].title}: ${ songs[i].artist}, ${ songs[i].album} (${ songs[i].year})
+        el.append(`<li class="dig">${ songs[i].title}: ${ songs[i].artist}, ${ songs[i].album} (${ songs[i].year})
         <button class="digButton">Dig</button></li>`);
     }
-    
-    
 }//end displaySongs.
 
 /// - test song
